68a3a17b9ccbade27c32e0849cb5ae36e6d8ae3c
Anis Karić <anis.karic19@gmail.com>
2024-10-15 13:04:31 +0200

    7 fix

--- oktobar/10/7.js
+++ oktobar/10/7.js
@@ -3,15 +3,17 @@
 
 function prosjecnaDuzinaRijeci(str){
     let br=0;
-    let max=0;
+    let avg=[];
     str=str.trim();
-    for(let i=0;i<str.length;i++){
-        if(str[i]==" " && max<br){max=br;br=0;}
-        else if(str[i]==" ")br=0;
+    for(let i=0;i<=str.length;i++){
+        if(str[i]==" " || i==str.length){avg.push(br);br=0;}
         else br++;
     }
 
-    return max;
+    let sum=0;
+    for(let i=0;i<avg.length;i++)sum+=avg[i];
+
+    return sum/avg.length;
 }
 
 
